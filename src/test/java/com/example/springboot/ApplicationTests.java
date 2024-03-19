fc115f824958a8a8d88b90ace1cbaa747bfe4eac
sullis <github@seansullivan.com>
2024-03-19 11:37:39 -0700

    add netty boringssl library

--- src/test/java/com/example/springboot/ApplicationTests.java
+++ src/test/java/com/example/springboot/ApplicationTests.java
@@ -1,10 +1,14 @@
 package com.example.springboot;
 
 import io.netty.channel.epoll.Epoll;
+import io.netty.handler.ssl.OpenSsl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class ApplicationTests {
@@ -13,6 +17,12 @@ class ApplicationTests {
 	@EnabledOnOs(value = { OS.LINUX })
 	void epollIsAvailableOnLinux() {
 		Epoll.ensureAvailability();
+	}
+
+	@Test
+	void boringSslIsAvailable() {
+		OpenSsl.ensureAvailability();
+		assertThat(OpenSsl.versionString()).isEqualTo("BoringSSL");
 	}
 
 	@Test
