adb7e21a708a782dab66bd6c808399165dddde4d
sullis <github@seansullivan.com>
2024-03-19 11:33:49 -0700

    add unit test for epoll on linux (#1)

--- src/test/java/com/example/springboot/ApplicationTests.java
+++ src/test/java/com/example/springboot/ApplicationTests.java
@@ -1,10 +1,19 @@
 package com.example.springboot;
 
+import io.netty.channel.epoll.Epoll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
+
+	@Test
+	@EnabledOnOs(value = { OS.LINUX })
+	void epollIsAvailableOnLinux() {
+		Epoll.ensureAvailability();
+	}
 
 	@Test
 	void contextLoads() {
