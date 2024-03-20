a3f867b807970c69ec72a1838f59540066cc0d82
sullis <github@seansullivan.com>
2024-03-19 19:04:17 -0700

    add HelloWorldController

--- src/test/java/com/example/springboot/ApplicationTests.java
+++ src/test/java/com/example/springboot/ApplicationTests.java
@@ -5,13 +5,23 @@ import io.netty.handler.ssl.OpenSsl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	@EnabledOnOs(value = { OS.LINUX })
@@ -26,7 +36,9 @@ class ApplicationTests {
 	}
 
 	@Test
-	void contextLoads() {
+	void serverReturnsHelloWorld() {
+		assertThat(restTemplate.getForObject("http://localhost:" + port + "/",
+				String.class)).contains("Hello world");
 	}
 
 }
